47eb547bbbdfbd11f03823a66ef5904b11f52719
git <853950441@qq.com>
2018-01-15 15:02:52 +0800

    'a'

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @Author: Administrator
+ * @Date:   2018-01-15 14:54:55
+ * @Last Modified by:   Administrator
+ * @Last Modified time: 2018-01-15 14:56:21
+ */
+inclued('views/a.html');
