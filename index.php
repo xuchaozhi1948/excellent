d76ab4dd968368bb7c76e49621aa1765e7281d7a
git <853950441@qq.com>
2018-01-15 15:40:05 +0800

    'a'

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
  * @Last Modified by:   Administrator
  * @Last Modified time: 2018-01-15 14:56:21
  */
-<<<<<<< HEAD
-include('views/a.html');
-=======
+
+
+
 
 include_once 'views/a.html';
->>>>>>> bdddf909471eacba9138ffed0ac25db341569233
+
